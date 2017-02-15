99b99ff50c2705e1e0b808013950f522dc5ce2c7
pcbreflux <pcbreflux@gmail.com>
2017-02-15 18:59:47 +0100

    I²C for u8g2 with ssd1306

---
+++ hardware/displays/U8G2/test_SSD1306_i2c.c
@@ -0,0 +1,59 @@
+#include <driver/gpio.h>
+#include <driver/spi_master.h>
+#include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <stdio.h>
+#include <string.h>
+#include <u8g2.h>
+
+#include "sdkconfig.h"
+#include "u8g2_esp32_hal.h"
+
+// SDA - GPIO21
+#define PIN_SDA 21
+
+// SCL - GPIO22
+#define PIN_SCL 22
+
+static const char *TAG = "ssd1306";
+
+void task_test_SSD1306i2c(void *ignore) {
+	u8g2_esp32_hal_t u8g2_esp32_hal = U8G2_ESP32_HAL_DEFAULT;
+	u8g2_esp32_hal.sda   = PIN_SDA;
+	u8g2_esp32_hal.scl  = PIN_SCL;
+	u8g2_esp32_hal_init(u8g2_esp32_hal);
+
+
+	u8g2_t u8g2; // a structure which will contain all the data for one display
+	u8g2_Setup_ssd1306_128x32_univision_f(
+		&u8g2,
+		U8G2_R0,
+		//u8x8_byte_sw_i2c,
+		u8g2_esp32_msg_i2c_cb,
+		u8g2_esp32_msg_i2c_and_delay_cb);  // init u8g2 structure
+	u8x8_SetI2CAddress(&u8g2.u8x8,0x78);
+
+	ESP_LOGI(TAG, "u8g2_InitDisplay");
+	u8g2_InitDisplay(&u8g2); // send init sequence to the display, display is in sleep mode after this,
+
+	ESP_LOGI(TAG, "u8g2_SetPowerSave");
+	u8g2_SetPowerSave(&u8g2, 0); // wake up display
+	ESP_LOGI(TAG, "u8g2_ClearBuffer");
+	u8g2_ClearBuffer(&u8g2);
+	ESP_LOGI(TAG, "u8g2_DrawBox");
+	u8g2_DrawBox(&u8g2, 0, 26, 80,6);
+	u8g2_DrawFrame(&u8g2, 0,26,100,6);
+
+	ESP_LOGI(TAG, "u8g2_SetFont");
+    u8g2_SetFont(&u8g2, u8g2_font_ncenB14_tr);
+	ESP_LOGI(TAG, "u8g2_DrawStr");
+    u8g2_DrawStr(&u8g2, 2,17,"Hi nkolban!");
+	ESP_LOGI(TAG, "u8g2_SendBuffer");
+	u8g2_SendBuffer(&u8g2);
+
+	ESP_LOGI(TAG, "All done!");
+
+	vTaskDelete(NULL);
+}
+
--- hardware/displays/U8G2/u8g2_esp32_hal.h
+++ hardware/displays/U8G2/u8g2_esp32_hal.h
@@ -7,22 +7,36 @@
 
 #ifndef U8G2_ESP32_HAL_H_
 #define U8G2_ESP32_HAL_H_
-#include <driver/gpio.h>
-#include <u8g2.h>
+#include "u8g2.h"
+
+#include "driver/gpio.h"
+#include "driver/spi_master.h"
+#include "driver/i2c.h"
 
 #define U8G2_ESP32_HAL_UNDEFINED (-1)
+
+#define I2C_MASTER_NUM I2C_NUM_1   /*!< I2C port number for master dev */
+#define I2C_MASTER_TX_BUF_DISABLE   0   /*!< I2C master do not need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE   0   /*!< I2C master do not need buffer */
+#define I2C_MASTER_FREQ_HZ    50000     /*!< I2C master clock frequency */
+#define ACK_CHECK_EN   0x1     /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS  0x0     /*!< I2C master will not check ack from slave */
 
 typedef struct {
 	gpio_num_t clk;
 	gpio_num_t mosi;
+	gpio_num_t sda; // data for I²C
+	gpio_num_t scl; // clock for I²C
 	gpio_num_t cs;
 	gpio_num_t reset;
 	gpio_num_t dc;
 } u8g2_esp32_hal_t ;
 
-#define U8G2_ESP32_HAL_DEFAULT {U8G2_ESP32_HAL_UNDEFINED, U8G2_ESP32_HAL_UNDEFINED, U8G2_ESP32_HAL_UNDEFINED, U8G2_ESP32_HAL_UNDEFINED, U8G2_ESP32_HAL_UNDEFINED }
+#define U8G2_ESP32_HAL_DEFAULT {U8G2_ESP32_HAL_UNDEFINED, U8G2_ESP32_HAL_UNDEFINED, U8G2_ESP32_HAL_UNDEFINED, U8G2_ESP32_HAL_UNDEFINED, U8G2_ESP32_HAL_UNDEFINED, U8G2_ESP32_HAL_UNDEFINED, U8G2_ESP32_HAL_UNDEFINED }
 
 void u8g2_esp32_hal_init(u8g2_esp32_hal_t u8g2_esp32_hal_param);
 uint8_t u8g2_esp32_msg_comms_cb(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
 uint8_t u8g2_esp32_msg_gpio_and_delay_cb(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
+uint8_t u8g2_esp32_msg_i2c_cb(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
+uint8_t u8g2_esp32_msg_i2c_and_delay_cb(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
 #endif /* U8G2_ESP32_HAL_H_ */
