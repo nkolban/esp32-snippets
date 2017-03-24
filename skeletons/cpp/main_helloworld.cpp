b15185bf096f905b1af36722d7f81ffe0afde5f2
kolban <kolban@us.ibm.com>
2017-03-23 23:47:47 -0500

    Thu Mar 23 23:47:47 CDT 2017

--- skeletons/cpp/main_helloworld.cpp
+++ skeletons/cpp/main_helloworld.cpp
@@ -1,6 +1,16 @@
+/*
+ * 1. Open up the project properties
+ * 2. Visit C/C++ General > Preprocessor Include Paths, Macros, etc
+ * 3. Select the Providers tab
+ * 4. Check the box for "CDT GCC Built-in Compiler Settings"
+ * 5. Set the compiler spec command to "xtensa-esp32-elf-gcc ${FLAGS} -E -P -v -dD "${INPUTS}""
+ * 6. Rebuild the index
+*/
+
 #include <esp_log.h>
 #include <string>
 #include "sdkconfig.h"
+
 
 static char tag[]="cpp_helloworld";
 
