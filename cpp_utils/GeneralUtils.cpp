6817fee198f017f746ea7fc2c283f8a38bd01a7b
Jeff Edson <jeff.edson@hotmail.com>
2018-04-23 16:30:55 -0700

    Added GeneralUtils::wifiErrorToString - Convert a wifi_err_reason_t code to a string

--- cpp_utils/GeneralUtils.cpp
+++ cpp_utils/GeneralUtils.cpp
@@ -434,6 +434,82 @@ const char* GeneralUtils::errorToString(esp_err_t errCode) {
 	return "Unknown ESP_ERR error";
 } // errorToString
 
+/**
+ * @brief Convert a wifi_err_reason_t code to a string.
+ * @param [in] errCode The errCode to be converted.
+ * @return A string representation of the error code.
+ * 
+ * @note: wifi_err_reason_t values as of April 2018 are: (1-24, 200-204) and are defined in ~/esp-idf/components/esp32/include/esp_wifi_types.h.
+ */
+const char* GeneralUtils::wifiErrorToString(uint8_t errCode) {
+  if (errCode == ESP_OK)
+    return "ESP_OK (received SYSTEM_EVENT_STA_GOT_IP event)";
+  if (errCode == UINT8_MAX)
+    return "Not Connected (default value)";
+
+	switch((wifi_err_reason_t) errCode) {
+	case WIFI_REASON_UNSPECIFIED:
+		return "WIFI_REASON_UNSPECIFIED";
+	case WIFI_REASON_AUTH_EXPIRE:
+		return "WIFI_REASON_AUTH_EXPIRE";
+	case WIFI_REASON_AUTH_LEAVE:
+		return "WIFI_REASON_AUTH_LEAVE";
+	case WIFI_REASON_ASSOC_EXPIRE:
+		return "WIFI_REASON_ASSOC_EXPIRE";
+	case WIFI_REASON_ASSOC_TOOMANY:
+		return "WIFI_REASON_ASSOC_TOOMANY";
+	case WIFI_REASON_NOT_AUTHED:
+		return "WIFI_REASON_NOT_AUTHED";
+	case WIFI_REASON_NOT_ASSOCED:
+		return "WIFI_REASON_NOT_ASSOCED";
+	case WIFI_REASON_ASSOC_LEAVE:
+		return "WIFI_REASON_ASSOC_LEAVE";
+	case WIFI_REASON_ASSOC_NOT_AUTHED:
+		return "WIFI_REASON_ASSOC_NOT_AUTHED";
+	case WIFI_REASON_DISASSOC_PWRCAP_BAD:
+		return "WIFI_REASON_DISASSOC_PWRCAP_BAD";
+	case WIFI_REASON_DISASSOC_SUPCHAN_BAD:
+		return "WIFI_REASON_DISASSOC_SUPCHAN_BAD";
+	case WIFI_REASON_IE_INVALID:
+		return "WIFI_REASON_IE_INVALID";
+	case WIFI_REASON_MIC_FAILURE:
+		return "WIFI_REASON_MIC_FAILURE";
+	case WIFI_REASON_4WAY_HANDSHAKE_TIMEOUT:
+		return "WIFI_REASON_4WAY_HANDSHAKE_TIMEOUT";
+	case WIFI_REASON_GROUP_KEY_UPDATE_TIMEOUT:
+		return "WIFI_REASON_GROUP_KEY_UPDATE_TIMEOUT";
+	case WIFI_REASON_IE_IN_4WAY_DIFFERS:
+		return "WIFI_REASON_IE_IN_4WAY_DIFFERS";
+	case WIFI_REASON_GROUP_CIPHER_INVALID:
+		return "WIFI_REASON_GROUP_CIPHER_INVALID";
+	case WIFI_REASON_PAIRWISE_CIPHER_INVALID:
+		return "WIFI_REASON_PAIRWISE_CIPHER_INVALID";
+	case WIFI_REASON_AKMP_INVALID:
+		return "WIFI_REASON_AKMP_INVALID";
+	case WIFI_REASON_UNSUPP_RSN_IE_VERSION:
+		return "WIFI_REASON_UNSUPP_RSN_IE_VERSION";
+	case WIFI_REASON_INVALID_RSN_IE_CAP:
+		return "WIFI_REASON_INVALID_RSN_IE_CAP";
+	case WIFI_REASON_802_1X_AUTH_FAILED:
+		return "WIFI_REASON_802_1X_AUTH_FAILED";
+	case WIFI_REASON_CIPHER_SUITE_REJECTED:
+		return "WIFI_REASON_CIPHER_SUITE_REJECTED";
+
+	case WIFI_REASON_BEACON_TIMEOUT:
+		return "WIFI_REASON_BEACON_TIMEOUT";
+	case WIFI_REASON_NO_AP_FOUND:
+		return "WIFI_REASON_NO_AP_FOUND";
+	case WIFI_REASON_AUTH_FAIL:
+		return "WIFI_REASON_AUTH_FAIL";
+	case WIFI_REASON_ASSOC_FAIL:
+		return "WIFI_REASON_ASSOC_FAIL";
+	case WIFI_REASON_HANDSHAKE_TIMEOUT:
+		return "WIFI_REASON_HANDSHAKE_TIMEOUT";
+	}
+	return "Unknown ESP_ERR error";
+} // wifiErrorToString
+
+
 
 /**
  * @brief Convert a string to lower case.
